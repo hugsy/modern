919e06033a1c29b2dbe10e1c4bf3e9ea98fb0bb7
hugsy <hugsy@blah.cat>
2021-08-23 17:55:43 -0700

    Auto add alias into shell startup script

--- modern_cmdline.py
+++ modern_cmdline.py
@@ -1,3 +1,11 @@
+"""
+This script will automatically download and install modern versions of the popular
+Unix tools (`ls`, `cat`, `wc`, etc.) and create aliases for them to be added directly
+to your shell startup file (bashrc, zshrc, Microsoft.PowerShell_profile.ps1, etc.).
+
+It also allows to create a markdown table with the tool names, along with their Windows
+compatibility and prebuild status.
+"""
 import argparse
 import glob
 import json
@@ -11,9 +19,23 @@ import sys
 import tempfile
 import zipfile
 
+
+DEFAULT_SCRIPT_NAME = os.path.basename(__file__)
+DEFAULT_OUTPUT_FILE = "./README.md"
 GIST_ROOT_URL = "https://gist.githubusercontent.com/hugsy/b950d6c98596c02cc129ead22dfb648c"
 GIST_URL = f"{GIST_ROOT_URL}/raw/487c78174b38a595c7e39a22a9a9a58e9690be77/info.json"
 GITHUB_API = "https://api.github.com"
+DEBUG = False
+
+
+class MissingPrebuildException(Exception): pass
+class UnsupportedOperatingSystemException(Exception): pass
+
+
+def dbg(x: str):
+    if DEBUG:
+        print(f"[*] {x}")
+    return
 
 
 def download_json_data(url: str) -> list[dict]:
@@ -51,7 +73,7 @@ def lookup_rust_tool_by_name(name: str) -> list[dict]:
     return lookup_tool_by_name(name, is_unix_tool = False, is_modern_tool = True)
 
 
-def create_table():
+def create_table(output_file: str = DEFAULT_OUTPUT_FILE):
     def lin_logo():
         return """<img title="Linux only" src=https://www.ximea.com/support/attachments/download/1160/linux_logo_small.png height=23px>"""
 
@@ -62,7 +84,7 @@ def create_table():
         return """<img title="Windows only" src="https://blog.thesysadmins.co.uk/wp-content/uploads/Windows-8-logo-100x100.jpg" height=23px> """
 
     js = collect_json_data()
-    with open("./rust_cmdline_tools_compat.md", "w", encoding="utf-8") as f:
+    with open(output_file, "w", encoding="utf-8") as f:
         f.write(f"| Unix tool | Rust version | Windows compatible? | Has prebuild? |\n")
         f.write(f"|:---:|:---:|:---:|:---:|\n")
 
@@ -75,9 +97,22 @@ def create_table():
                 if o == "mac": prebuild_list.append(mac_logo())
 
             f.write(f'| `{tool["unix-tool"]}` | [`{tool["modern-tool"]}`]({tool["url"]}) | {is_windows_compatible} | {" ".join(prebuild_list)} |\n')
+    return
 
 
 def download_latest_release(tool: dict) -> pathlib.Path:
+    __os = platform.system().lower()
+    __arch = platform.architecture()[0]
+    possible_oses = []
+    if __os == "windows": possible_oses = ("windows", "msvc")
+    possible_arches = []
+    if __arch == "64bit": possible_arches = ("amd64", "x86_64")
+
+    if (__os == "windows" and "win" not in tool["prebuild"]) \
+        or (__os == "linux" and "lin" not in tool["prebuild"]) \
+        or (__os == "macos" and "mac" not in tool["prebuild"]):
+            raise MissingPrebuildException(f"Tool {tool['unix-tool']} is not available for {__os}")
+
     gh_tool_author, gh_tool_name = tool["url"].replace("https://github.com/", "").split("/")
 
     # download the data
@@ -86,26 +121,21 @@ def download_latest_release(tool: dict) -> pathlib.Path:
     # find the right asset in the latest release
     latest_release = release_js[0]
     assets = latest_release["assets"]
-    __os = platform.system().lower()
-    possible_oses = []
-    if __os == "windows": possible_oses = ("windows", "msvc")
-
-    arch = platform.architecture()[0]
-    possible_arches = []
-    if arch == "64bit": possible_arches = ("amd64", "x86_64")
 
     match = None
     for asset in assets:
         for o in possible_oses:
             for a in possible_arches:
+                dbg(f"trying {a}/{o} for {asset['name']}")
                 if o in asset["name"].lower() and a in asset["name"].lower():
                     match = asset
+                    dbg(f"match found: '{match['name'].lower()}'")
                     break
             if match: break
         if match: break
 
     if not match:
-        raise Exception(f"No asset found for {__os} {arch}")
+        raise Exception(f"No asset found for {__os} {__arch}")
 
     # download the asset in tempdir
     h = requests.get(match["browser_download_url"])
@@ -152,20 +182,33 @@ def install(tool: dict) -> int:
 
 
     # 1. check & download the latest release for the current OS
-    tmpbin = download_latest_release(tool)
+    try:
+        tmpbin = download_latest_release(tool)
+    except MissingPrebuildException as e:
+        print(f"Exception raised: {str(e)}")
+        print(f"Trying running with `-c` instead...")
+        raise e
 
     # 2. copy the file to output directory
     shutil.copy(tmpbin, output_directory)
 
     # 3. print the aliasing command for the tool for the current OS
-    print("------8<------")
     if is_windows:
-        install_path = output_directory.absolute() / tool['modern-tool']+".exe"
-        print(f"New-Alias -Name {tool['unix-tool']} -Value '{install_path}' -Option ReadOnly")
+        alias_file = pathlib.Path(home) / "Documents" / "WindowsPowerShell" / "Microsoft.PowerShell_profile.ps1"
+        install_path = output_directory.absolute() / (tool['modern-tool']+".exe")
+        line_to_add = f"New-Alias -Name {tool['unix-tool']} -Value '{install_path}' -Option ReadOnly # added by {DEFAULT_SCRIPT_NAME}"
     else:
+        alias_file = pathlib.Path(home) / ".aliases"
         install_path = output_directory.absolute() / tool['modern-tool']
-        print(f"alias {tool['unix-tool']}='{install_path}'")
-    print("------>8------")
+        line_to_add = f"alias {tool['unix-tool']}='{install_path}' # added by {DEFAULT_SCRIPT_NAME}"
+
+    if alias_file.is_file():
+        if line_to_add not in open(str(alias_file.absolute()), "r").readlines():
+            with open(str(alias_file.absolute()), "a") as f:
+                f.write(f"{line_to_add} {os.linesep}")
+            print(f"Alias added to `{alias_file}`")
+        else:
+            print(f"Alias to `{tool['modern-tool']}` already in `{alias_file}`")
     return 0
 
 
@@ -179,10 +222,15 @@ if __name__ == "__main__":
     install_group = parser.add_argument_group("install table")
     install_group.add_argument("-u", "--search-unix", help="Search for a unix tool", type=str, metavar="NAME")
     install_group.add_argument("-r", "--search-rust", help="Search for a rust tool", type=str, metavar="NAME")
-    install_group.add_argument("-s", "--switch", help="Download and install a legacy unix tool with a rust tool", type=str, metavar="RUST_TOOL_NAME")
-    install_group.add_argument("-S", "--switch-all", help="Download and install all legacy unix tool with rust tools", action="store_true")
+    install_group.add_argument("-s", "--switch", help="Download prebuild and install a legacy unix tool with a rust tool", type=str, metavar="RUST_TOOL_NAME")
+    install_group.add_argument("-S", "--switch-all", help="Download prebuild and install all legacy unix tool with rust tools", action="store_true")
+    install_group.add_argument("-c", "--compile", help="Download, compile and install a legacy unix tool with a rust tool", type=str, metavar="RUST_TOOL_NAME")
+    install_group.add_argument("-C", "--compile-all", help="Download, compile and install all legacy unix tool with rust tools", action="store_true")
 
     args = parser.parse_args()
+
+    if args.verbose:
+        DEBUG = True
 
     if args.generate_table:
         create_table()
@@ -208,13 +256,21 @@ if __name__ == "__main__":
 
     if args.switch_all:
         js = collect_json_data()
+        retcode = 0
         for entry in js:
             res = lookup_rust_tool_by_name(entry["modern-tool"])
             if len(res) != 1:
                 print("/!\\ Found multiple matches, cannot proceed /!\\")
-                break
+                continue
 
             tool = res[0]
             if install(tool) != 0:
                 print(f"/!\\ Error installing tool '{tool}' /!\\")
-                break
+                retcode += 1
+        exit(retcode)
+
+    if args.compile:
+        raise NotImplementedError("Compiling is not yet implemented")
+
+    if args.compile_all:
+        raise NotImplementedError("Compiling is not yet implemented")
